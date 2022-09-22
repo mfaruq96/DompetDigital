8debe74df633b2ca0a81366212168bf397eb61a6
Muhammad Faruq <muhammadfaruq.tkj1@gmail.com>
2022-09-22 21:25:29 +0700

    update login n register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,12 +5,20 @@ class Auth extends CI_Controller
 {
 	public function index()
 	{
+		$data['title'] = "Login";
+
+		$this->load->view('layouts/auth_header', $data);
 		$this->load->view('auth/login');
+		$this->load->view('layouts/auth_footer');
 	}
 
 	public function register()
 	{
+		$data['title'] = "Register";
+
+		$this->load->view('layouts/auth_header', $data);
 		$this->load->view('auth/register');
+		$this->load->view('layouts/auth_footer');
 	}
 	
 }
